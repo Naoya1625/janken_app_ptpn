b5eb4a3efbf955da42589bc3dea8d0685088c008
EC2 Default User <ec2-user@ip-172-31-9-120.us-east-2.compute.internal>
2020-12-22 05:50:57 +0000

    finish janken-app

---
+++ janken.rb
@@ -0,0 +1,86 @@
+# あっち向いてホイの判定を行うメソッド
+def result(winer) 
+  if ( @your_hoi_name == @partners_hoi_name)
+    puts "あなた:「#{@your_hoi_name}」 相手:「#{@partners_hoi_name}」"
+    puts "#{winer}の勝利です！"
+    exit
+  else
+    puts "あなた:「#{@your_hoi_name}」 相手:「#{@partners_hoi_name}」"
+    puts "もう一度最初から！\n--------------------------"
+    janken
+  end
+end
+
+# あっち向いてホイを行うメソッド
+# flag: 0=勝ち,１=あいこ,２=負け
+def hoi(flag)
+  puts "あっちむいて....."
+  puts "0(上), 1(右), 2(左), 3(右)"
+  your_hoi = gets.chomp
+  
+  # 選択肢以外を選択した場合もう一度入力してもらう。
+  until your_hoi == "0" || your_hoi == "1" || your_hoi == "2" || your_hoi == "3"
+    puts "もう！真面目にやってくださいね！\nもう一度入力してください!"
+    puts "あっちむいて.....\n"
+    puts "0(上), 1(右), 2(左), 3(右)"
+    your_hoi = gets.chomp
+  end
+  
+  partners_hoi = [ 0 ,1, 2, 3 ].sample
+  hoi_all = [ "上", "下", "左", "右" ]
+  @your_hoi_name = hoi_all[your_hoi.to_i]
+  @partners_hoi_name = hoi_all[partners_hoi]
+  
+  case flag
+    when 0  # あなたがジャンケンに勝った場合のあっち向いてホイ
+      result("あなた")
+    when 1  # あなたがジャンケンに負けた場合のあっち向いてホイ
+      result("相手")
+  end
+end
+
+# じゃんけんを行うメソッド
+def janken(message = "じゃんけん......\n ")
+  puts message
+  puts "0(グー) 1(チョキ) 2(パー) 3(戦わない)"
+  your_hand = gets.chomp.to_s
+  puts "--------------------------"
+  partners_hand = %w[ 0 1 2 ].sample
+  
+  # 選択肢以外の入力をした場合にもう一度入力してもらう。
+  until your_hand == "0" || your_hand == "1" || your_hand == "2" || your_hand == "3"
+    puts "そんな選択肢はありませんよ！\nもう一度入力してください！\nじゃんけん......\n "
+    puts "0(グー) 1(チョキ) 2(パー) 3(戦わない)"
+    your_hand = gets.chomp.to_s
+  end
+  
+  weapons = [ "グー", "チョキ", "パー" ]
+  
+  if (your_hand == "0" && partners_hand == "1") || (your_hand == "1" && partners_hand == "2") || (your_hand == "2" && partners_hand == "0") 
+    # あなたがじゃんけんに勝利した場合
+    puts "あなた:#{weapons[your_hand.to_i]}"
+    puts "相手:#{weapons[partners_hand.to_i]}\n"
+    puts "あなたはジャンケンに勝利しました！\n次はあっち向いてホイだ！"
+    hoi_flag = 0
+    hoi(hoi_flag)
+    
+  elsif your_hand == partners_hand  # あいこの場合  
+    janken("あいこです！\nあいこで......\n ")
+  
+  else
+    if your_hand == "3"  # 戦わないことを選んだ場合
+      puts "戦わないことを選んだため、あなたは負けました!"
+      exit
+      
+    else  # あなたがじゃんけんで負けた場合
+      puts "あなた:#{weapons[your_hand.to_i]}"
+      puts "相手:#{weapons[partners_hand.to_i]}\n"
+      puts "あなたはジャンケンに負けました。\n次はあっち向いてホイだ！"
+      hoi_flag = 1
+      hoi(hoi_flag)
+      exit
+    end
+  end
+end
+
+janken
